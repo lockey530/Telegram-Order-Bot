c6bd589803a2f3f3b4a51c7b6a262314e3148280
lockey530 <162585372+lockey530@users.noreply.github.com>
2024-10-25 01:12:51 +0800

    Update OrderBot.py

--- OrderBot.py
+++ OrderBot.py
@@ -7,13 +7,15 @@ from threading import Lock  # For thread safety
 bot = telebot.TeleBot(config.TOKEN)
 
 # List of admin chat IDs
-ADMIN_CHAT_IDS = [551429608, 881189472]
+ADMIN_CHAT_IDS = [551429608, 881189472]  # Add more admin IDs if needed
 
-# Updated menu structure
+# Updated drink menu
 menu = {
-    "Soft Drinks": ["Coca-Cola", "Coca-Cola Zero", "Sprite", "F&N Orange", "Coffee", "Tea"],
     "Liquor": ["Tiger Beer", "Vodka", "Whiskey", "Rum", "Gin"]
 }
+
+# Mixer options
+mixers = ["Coca-Cola", "Coca-Cola Zero", "Sprite", "F&N Orange", "No Mixer"]
 
 # Store each user's order and state
 user_data = {}
@@ -57,50 +59,20 @@ def welcome(message):
     menu_msg = bot.send_photo(chat_id, MENU_IMAGE_FILE_ID)
     user_data[chat_id]["message_ids"].append(menu_msg.message_id)
 
-    ask_question(message, 0)
+    show_drink_menu(message)
 
-def ask_question(message, question_index):
+def show_drink_menu(message):
     chat_id = message.chat.id
-    questions = ["Please enter your name:", "Please enter your Telegram handle:"]
-
-    if question_index < len(questions):
-        msg = bot.send_message(chat_id, questions[question_index])
-        user_data[chat_id]["message_ids"].append(msg.message_id)
-        bot.register_next_step_handler(msg, handle_answer, question_index)
-    else:
-        show_category_menu(message)
-
-def handle_answer(message, question_index):
-    chat_id = message.chat.id
-    user_data[chat_id]["answers"].append(message.text)
-    user_data[chat_id]["message_ids"].append(message.message_id)
-    ask_question(message, question_index + 1)
-
-def show_category_menu(message):
-    chat_id = message.chat.id
-    user_data[chat_id]["state"] = "CHOOSING_CATEGORY"
-
-    markup = types.InlineKeyboardMarkup()
-    for category in menu.keys():
-        markup.add(types.InlineKeyboardButton(category, callback_data=category))
-
-    msg = bot.send_message(chat_id, "Please select a category:", reply_markup=markup)
-    user_data[chat_id]["message_ids"].append(msg.message_id)
-
-@bot.callback_query_handler(func=lambda call: call.data in menu.keys())
-def show_drink_menu(call):
-    chat_id = call.message.chat.id
-    category = call.data
     user_data[chat_id]["state"] = "CHOOSING_DRINK"
-    user_data[chat_id]["selected_category"] = category
 
     markup = types.InlineKeyboardMarkup()
-    for drink in menu[category]:
+    for drink in menu["Liquor"]:
         markup.add(types.InlineKeyboardButton(drink, callback_data=drink))
 
-    bot.edit_message_text("Please select a drink:", chat_id, call.message.message_id, reply_markup=markup)
+    msg = bot.send_message(chat_id, "Please select a drink:", reply_markup=markup)
+    user_data[chat_id]["message_ids"].append(msg.message_id)
 
-@bot.callback_query_handler(func=lambda call: any(call.data in drinks for drinks in menu.values()))
+@bot.callback_query_handler(func=lambda call: call.data in menu["Liquor"])
 def handle_drink_selection(call):
     chat_id = call.message.chat.id
     selected_drink = call.data
@@ -108,19 +80,15 @@ def handle_drink_selection(call):
 
     bot.edit_message_reply_markup(chat_id, call.message.message_id, reply_markup=None)
 
-    if user_data[chat_id]["selected_category"] == "Liquor":
-        ask_for_mixer(chat_id, selected_drink)
-    else:
-        ask_quantity(call.message, selected_drink)
+    ask_for_mixer(chat_id, selected_drink)
 
 def ask_for_mixer(chat_id, selected_drink):
     user_data[chat_id]["state"] = "CHOOSING_MIXER"
     user_data[chat_id]["selected_drink"] = selected_drink
 
     markup = types.InlineKeyboardMarkup()
-    for mixer in menu["Soft Drinks"]:
+    for mixer in mixers:
         markup.add(types.InlineKeyboardButton(mixer, callback_data=f"mixer_{mixer}"))
-    markup.add(types.InlineKeyboardButton("No Mixer", callback_data="mixer_None"))
 
     msg = bot.send_message(chat_id, "Please select a mixer:", reply_markup=markup)
     user_data[chat_id]["message_ids"].append(msg.message_id)
@@ -131,7 +99,7 @@ def handle_mixer_selection(call):
     mixer = call.data.split("_")[1]
     selected_drink = user_data[chat_id]["selected_drink"]
 
-    if mixer == "None":
+    if mixer == "No Mixer":
         combined_order = selected_drink
     else:
         combined_order = f"{selected_drink} with {mixer}"
@@ -173,7 +141,7 @@ def handle_quantity_selection(message, selected_drink):
 def handle_more_drinks(call):
     chat_id = call.message.chat.id
     if call.data == "yes_more_drinks":
-        show_category_menu(call.message)
+        show_drink_menu(call.message)
     else:
         if user_data[chat_id]["state"] != "FINALIZED":
             finalize_order(call.message)
